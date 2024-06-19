8eeaf1139cb9a1d479c589c4ca959e98b5dac134
jerry-yu <anhuitianyu@163.com>
2024-06-19 10:38:56 +0800

    add mint dpr to user who claiming

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ pub mod instruction;
 mod process;
 pub mod state;
 
-
 pub const TOKEN_SEED: &[u8] = b"dpr_token";
 pub const CREDIT_SETTING_SEED: &[u8] = b"credit_setting";
 pub const USER_CREDIT_SEED: &[u8] = b"user";
+pub const MINT_AUTHORITY_SEED: &[u8] = b"mint_authority";
--- src/process.rs
+++ src/process.rs
@@ -1,6 +1,3 @@
-use crate::instruction::CreditInstruction;
-use crate::state::{CreditSettings, UserCredit};
-use crate::state::{PrivelegeUser, TokenAccount, UserAccount};
 use borsh::{to_vec, BorshDeserialize, BorshSerialize};
 use solana_program::{
     account_info::{next_account_info, AccountInfo},
@@ -13,7 +10,15 @@ use solana_program::{
     system_instruction,
     sysvar::{clock, rent::Rent, Sysvar},
 };
-use crate::{TOKEN_SEED,CREDIT_SETTING_SEED,USER_CREDIT_SEED};
+use spl_associated_token_account::{
+    get_associated_token_address, instruction as associated_token_account_instruction,
+};
+use spl_token::instruction as token_instruction;
+
+use crate::{instruction::CreditInstruction, MINT_AUTHORITY_SEED};
+use crate::state::{CreditSettings, UserCredit};
+use crate::state::{PrivelegeUser, TokenAccount, UserAccount};
+use crate::{CREDIT_SETTING_SEED, TOKEN_SEED, USER_CREDIT_SEED};
 
 const SECS_PER_DAY: i64 = 60 * 60 * 24;
 
@@ -25,9 +30,9 @@ pub fn process_instruction(
     let instruction = CreditInstruction::try_from_slice(instruction_data)?;
 
     match instruction {
-        CreditInstruction::Init { settings, token } => {
+        CreditInstruction::Init { settings, token} => {
             // Initialize PDA with credit value
-            process_init(program_id, accounts, settings, token)?;
+            process_init(program_id, accounts, settings, token,)?;
         }
         CreditInstruction::Add {
             pk,
@@ -35,7 +40,7 @@ pub fn process_instruction(
             campaign,
             reward_since,
         } => {
-            process_credit(program_id, accounts, pk, credit, campaign,reward_since)?;
+            process_credit(program_id, accounts, pk, credit, campaign, reward_since)?;
         }
         CreditInstruction::SetTokenAddress { address } => {
             process_token_address(program_id, accounts, address)?;
@@ -60,6 +65,8 @@ pub(crate) fn process_init(
     //let user_account = next_account_info(accounts_iter)?;
     let setting_account = next_account_info(accounts_iter)?;
     let dpr_account = next_account_info(accounts_iter)?;
+    let mint_authority = next_account_info(accounts_iter)?;
+
     let system_program = next_account_info(accounts_iter)?;
 
     let (setting_key, setting_seed) =
@@ -70,6 +77,11 @@ pub(crate) fn process_init(
 
     let (dpr_key, dpr_seed) = Pubkey::find_program_address(&[TOKEN_SEED], program_id);
     if dpr_key != *dpr_account.key {
+        return Err(ProgramError::InvalidArgument);
+    }
+
+    let (mint_key, mint_seed) = Pubkey::find_program_address(&[MINT_AUTHORITY_SEED], program_id);
+    if mint_key != *mint_authority.key {
         return Err(ProgramError::InvalidArgument);
     }
 
@@ -131,9 +143,28 @@ pub(crate) fn process_init(
     let mut data = dpr_account.data.borrow_mut();
     data.copy_from_slice(&token_data);
 
+    let required_lamports = rent.minimum_balance(0);
+    let create_account_ix = system_instruction::create_account(
+        payer_account.key,
+        mint_authority.key,
+        required_lamports,
+        0,
+        program_id,
+    );
+
+    invoke_signed(
+        &create_account_ix,
+        &[
+            payer_account.clone(),
+            mint_authority.clone(),
+            system_program.clone(),
+        ],
+        &[&[MINT_AUTHORITY_SEED, &[mint_seed]]],
+    )?;
+
     msg!(
-        "PDA account initialized with credit setting: {:?}",
-        settings
+        "PDA account initialized with credit setting: {:?} mint_authority {:?}",
+        settings,mint_authority
     );
 
     Ok(())
@@ -157,7 +188,7 @@ pub(crate) fn process_credit(
     key: Pubkey,
     credit_value: i32,
     campaign_id: u16,
-    reward_since:u32,
+    reward_since: u32,
 ) -> ProgramResult {
     let accounts_iter = &mut accounts.iter();
     let payer_account = next_account_info(accounts_iter)?;
@@ -315,24 +346,79 @@ fn calculate_current_earnings(
 pub(crate) fn process_claim(program_id: &Pubkey, accounts: &[AccountInfo]) -> ProgramResult {
     let accounts_iter = &mut accounts.iter();
     let payer_account = next_account_info(accounts_iter)?;
+    let mint_authority = next_account_info(accounts_iter)?;
     let user_credit_account = next_account_info(accounts_iter)?;
+
+    let user_associated_token_account = next_account_info(accounts_iter)?;
+
     let token_account = next_account_info(accounts_iter)?;
+    let dpr_mint_account = next_account_info(accounts_iter)?;
     let setting_account = next_account_info(accounts_iter)?;
+
     let system_program = next_account_info(accounts_iter)?;
+    let token_program = next_account_info(accounts_iter)?;
+    let associated_token_program = next_account_info(accounts_iter)?;
+    let my_program = next_account_info(accounts_iter)?;
+
+    msg!(
+        "======== {:?} {:?} {:?} {:?} {:?} {:?}",
+        dpr_mint_account.key,
+        token_account.key,
+        associated_token_program.key,
+        my_program.key,
+        system_program.key,
+        token_program.key,
+    );
+
+    let expected_associated_token_address =
+        get_associated_token_address(&payer_account.key, &dpr_mint_account.key);
+
+    msg!(
+        "&&&&&&&&&&&& {:?}  {:?}",
+        expected_associated_token_address,
+        user_associated_token_account.key
+    );
+
+    if *user_associated_token_account.key != expected_associated_token_address {
+        return Err(ProgramError::InvalidArgument);
+    }
+
+    if user_associated_token_account.lamports() == 0 {
+        msg!("Creating associated token account...");
+        invoke(
+            &associated_token_account_instruction::create_associated_token_account(
+                payer_account.key,
+                payer_account.key,
+                dpr_mint_account.key,
+                token_program.key,
+            ),
+            &[
+                dpr_mint_account.clone(),
+                user_associated_token_account.clone(),
+                payer_account.clone(),
+                system_program.clone(),
+                token_program.clone(),
+                associated_token_program.clone(),
+            ],
+        )?;
+    } else {
+        msg!("Associated token account exists.");
+    }
 
     let (dpr_key, _) = Pubkey::find_program_address(&[TOKEN_SEED], program_id);
     let (user_credit_key, _) =
         Pubkey::find_program_address(&[USER_CREDIT_SEED, payer_account.key.as_ref()], program_id);
     let (setting_key, __) = Pubkey::find_program_address(&[CREDIT_SETTING_SEED], program_id);
+    let (mint_key, mint_seed) = Pubkey::find_program_address(&[MINT_AUTHORITY_SEED], program_id);
 
     msg!(
-        "---- {:?} {:?} {:?} {:?} {:?} {:?}",
+        "---- {:?} {:?} {:?} {:?} {:?} {:?}  {:?}",
         dpr_key,
         token_account.key,
         user_credit_key,
         user_credit_account.key,
         setting_key,
-        setting_account.key
+        setting_account.key,mint_key
     );
 
     if dpr_key != *token_account.key
@@ -344,6 +430,9 @@ pub(crate) fn process_claim(program_id: &Pubkey, accounts: &[AccountInfo]) -> Pr
 
     let settings = CreditSettings::try_from_slice(&setting_account.data.borrow())?;
     let token = TokenAccount::try_from_slice(&token_account.data.borrow())?;
+    if token.token != *dpr_mint_account.key {
+        return Err(ProgramError::InvalidArgument);
+    }
 
     let user_account = UserAccount::unpack(&mut &mut user_credit_account.data.borrow_mut()[..])?;
     msg!(
@@ -356,5 +445,23 @@ pub(crate) fn process_claim(program_id: &Pubkey, accounts: &[AccountInfo]) -> Pr
     let reward = calculate_current_earnings(&settings, &user_account, day as u32);
 
     msg!("getting reward {}", reward);
+
+    invoke_signed(
+        &token_instruction::mint_to(
+            token_program.key,
+            dpr_mint_account.key,
+            user_associated_token_account.key,
+            mint_authority.key,
+            &[mint_authority.key],
+            reward,
+        )?,
+        &[
+            dpr_mint_account.clone(),
+            mint_authority.clone(),
+            user_associated_token_account.clone(),
+            token_program.clone(),
+        ],
+        &[&[MINT_AUTHORITY_SEED, &[mint_seed]]],
+    )?;
     Ok(())
 }
